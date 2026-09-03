804b92e54ccc5638f74bebf0db3c5a9aa6dc55da
zhouqixing7012 <75143975+zhouqixing7012@users.noreply.github.com>
2026-09-03 20:59:13 +0800

    feat: add shadcn separator component

---
+++ src/components/ui/separator.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as SeparatorPrimitive from '@radix-ui/react-separator';
+
+import { cn } from '@/lib/utils';
+
+function Separator({ className, orientation = 'horizontal', decorative = true, ...props }) {
+  return (
+    <SeparatorPrimitive.Root
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        'shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Separator };
